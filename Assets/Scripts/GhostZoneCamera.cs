b23ac893e04e370701d1be6f282514f87071f916
MayaOsama <35902575+MayaOsama@users.noreply.github.com>
2019-11-08 12:16:04 +0200

    uploading 2 virtual cameras

---
+++ Assets/Scripts/GhostZoneCamera.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostZoneCamera : MonoBehaviour
+{
+    Transform transform;
+    float timer;
+   // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        timer = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0 )
+        {
+            timer -= Time.deltaTime;
+            transform.Translate(0, 0, -0.5f);
+        }
+    }
+}
---
+++ Assets/Scripts/ScanCityCameraScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScanCityCameraScript : MonoBehaviour
+{
+    Transform transform;
+    float timer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        timer = 30;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            transform.Translate(0, 0,2);
+        }
+    }
+}
