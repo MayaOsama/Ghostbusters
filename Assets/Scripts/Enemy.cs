f47ffc23b2d39d190fda62502b044d68e707f017
AhmedYasser97 <ahmed.yasserelsherif@student.guc.edu.eg>
2019-11-16 20:30:44 +0200

    YOU BETTER WORK

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    static Animator anim;
+    public float timer = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int seconds = (int)timer % 60;
+
+        if (seconds == 15)
+        {
+            anim.SetTrigger("isShooting");
+
+        }
+
+    }
+}
